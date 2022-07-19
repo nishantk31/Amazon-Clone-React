0e13317e8dc81f449cf2dcfaca87501ae08589d5
Nishant Kohale <89677821+nishantk31@users.noreply.github.com>
2022-07-19 16:32:43 +0530

    Add files via upload

---
+++ Amazon clone/src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div class="banner">
+      <img class="banner_img" src="images/banner.png" alt="" />
+    </div>
+  );
+}
+
+export default Banner;
